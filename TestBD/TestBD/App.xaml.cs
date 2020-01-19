10cececce619137f291927e718543b1733964635
Rubén Alba <ruben.alba@birchmangroup.com>
2020-01-20 00:50:45 +0100

    añadida metricas de appcenter

--- TestBD/TestBD/App.xaml.cs
+++ TestBD/TestBD/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace TestBD
 {
@@ -10,11 +13,15 @@ namespace TestBD
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomeViewPage();
         }
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=01e78b30-5ad8-474a-b56b-23d2c51be0b8;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
