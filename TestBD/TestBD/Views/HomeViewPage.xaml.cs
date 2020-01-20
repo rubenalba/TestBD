5492f3e591313746dc1dc9fc6acce0321240f625
Ruben Alba Revilla <ruben.alba@viseo.com>
2020-01-20 17:41:38 +0100

    implementado function

--- TestBD/TestBD/Views/HomeViewPage.xaml.cs
+++ TestBD/TestBD/Views/HomeViewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace TestBD
 {
@@ -8,8 +10,17 @@ namespace TestBD
         public HomeViewPage()
         {
             InitializeComponent();
-        }
+            usersList.ItemsSource = new List<string>();
 
-        
+            getUsersBtn.Clicked += async (s, e) =>
+            {
+                var client = new System.Net.Http.HttpClient();
+                string url = "https://rubenalbausers.azurewebsites.net/api/Users?code=QPBzkwsmhcV2qt3kCVSLXdPYt0t0bxv/Y2AJDI555sMIjfCWDb9u3w==";
+                var response = await client.GetAsync(url);
+                var usersJson = response.Content.ReadAsStringAsync().Result;
+                List<string> listUsers = JsonConvert.DeserializeObject<List<string>>(usersJson);
+                usersList.ItemsSource = listUsers;
+            };
+        }
     }
 }
